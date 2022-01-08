828cf9df80a7e81f933d202a12ebc4ab050b9415
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-08 21:01:26 +0100

    base views

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, "name": "Let's learn Python!"},
+    {'id': 2, "name": "Design with me"},
+    {'id': 3, "name": "Front-end Developers"},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html", {"rooms": rooms})
 
 
 def room(request):
