26dd913ebab64f871a4c3a04d3dc97464410b276
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-09 13:36:28 +0100

    Message model

--- base/models.py
+++ base/models.py
@@ -8,3 +8,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    # one to many relationship -> if room is deleted all messages are erased
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
